517e4de86057927e518fef1676c8355c4923ef7b
Dale Follett <58715445+twisted-builds-llc@users.noreply.github.com>
2024-10-14 20:35:16 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-#include "CustomDoubleSpinBox.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Use the system's native theme
+    a.setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
     return a.exec();
